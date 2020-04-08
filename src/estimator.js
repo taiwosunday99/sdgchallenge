04122dbc24a3caea3d174278d8e5409c2925da85
victor marius <taiwosunday99@gmail.com>
2020-04-08 20:51:19 +0100

    buildchangesmade

--- src/estimator.js
+++ src/estimator.js
@@ -1,26 +1,18 @@
-
 const estimateCurrentlyInfectedInDays = (reportedCases, timeToElapse) => {
-//   const timeToElapse = 58;
   const currentlyInfected = reportedCases * 10;
   const infectionsByRequestedTime = currentlyInfected * 1024;
   return infectionsByRequestedTime * timeToElapse * 1;
 };
-
 const estimateProjectedInfectionsInWeeks = (reportedCases, timeToElapse) => {
-//   const timeToElapse = 58;
   const currentlyInfected = reportedCases * 50;
   const infectionsByRequestedTime = currentlyInfected * 1024;
   return infectionsByRequestedTime * timeToElapse * 7;
 };
-
 const estimateProjectedInfectionsInMonths = (reportedCases, timeToElapse) => {
-//   const timeToElapse = 58;
   const currentlyInfected = reportedCases * 50;
   const infectionsByRequestedTime = currentlyInfected * 1024;
   return infectionsByRequestedTime * timeToElapse * 30;
 };
-
-
 const covid19ImpactEstimator = (data) => {
   const estimator = (
     // challenge 1
@@ -33,7 +25,6 @@ const covid19ImpactEstimator = (data) => {
     impact: {},
     severeImpact: {}
   });
-
 };
 
 export default covid19ImpactEstimator;
