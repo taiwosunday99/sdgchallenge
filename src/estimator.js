b76375e91b467d83a78945c450e9ba9e46fbdaf1
victor marius <taiwosunday99@gmail.com>
2020-04-08 09:45:27 +0100

    build challenges

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,10 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+  const { reportedCases } = data;
+  return {
+    data: reportedCases,
+    impact: { currentlyInfected: reportedCases * 10 },
+    severeImpact: { currentlyInfected: reportedCases * 50 }
+  };
+};
 
 export default covid19ImpactEstimator;
