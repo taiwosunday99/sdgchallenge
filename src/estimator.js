8f2d730a412a1e8f92267b5933ef57cda3a3e8a7
victor marius <taiwosunday99@gmail.com>
2020-04-08 18:04:37 +0100

    buildchallengeestimations

--- src/estimator.js
+++ src/estimator.js
@@ -1,29 +1,49 @@
-const estimateCurrentlyInfected = (reportedCases) => {
+const estimateCurrentlyInfectedInDays = (reportedCases) => {
   const currentlyInfected = reportedCases * 10;
   const infectionsByRequestedTime = currentlyInfected * 1024;
-  return infectionsByRequestedTime;
+  return infectionsByRequestedTime * 1;
 };
 
-const estimateProjectedInfections = (reportedCases) => {
+const estimateProjectedInfectionsInWeeks = (reportedCases) => {
   const currentlyInfected = reportedCases * 50;
   const infectionsByRequestedTime = currentlyInfected * 1024;
-  return infectionsByRequestedTime;
+  return infectionsByRequestedTime * 7;
+};
+
+const estimateProjectedInfectionsInMonths = (reportedCases) => {
+  const currentlyInfected = reportedCases * 50;
+  const infectionsByRequestedTime = currentlyInfected * 1024;
+  return infectionsByRequestedTime * 30;
 };
 
 
 const covid19ImpactEstimator = (data) => {
-  const input = data;
-  return {
-    data: input,
-    impact: {
-      currentlyInfected: estimateCurrentlyInfected.currentlyInfected,
-      infectionsByRequestedTime: estimateCurrentlyInfected.infectionsByRequestedTime
-    },
-    severeImpact: {
-      currentlyInfected: estimateProjectedInfections.currentlyInfected,
-      infectionsByRequestedTime: estimateProjectedInfections.infectionsByRequestedTime
-    }
-  };
+  const estimator = (
+    // challenge 1
+    estimateCurrentlyInfectedInDays,
+    estimateProjectedInfectionsInWeeks,
+    estimateProjectedInfectionsInMonths
+  );
+  return estimator({
+    data,
+    impact: {},
+    severeImpact: {}
+  });
+
 };
 
 export default covid19ImpactEstimator;
+
+
+//   const input = data;
+//   return {
+//     data: input,
+//     impact: {
+//       currentlyInfected: estimateCurrentlyInfected.currentlyInfected,
+//       infectionsByRequestedTime: estimateCurrentlyInfected.infectionsByRequestedTime
+//     },
+//     severeImpact: {
+//       currentlyInfected: estimateProjectedInfections.currentlyInfected,
+//       infectionsByRequestedTime: estimateProjectedInfections.infectionsByRequestedTime
+//     }
+//   }
