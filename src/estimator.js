4cc818f9ae808a05fe92bc56139e0dcff0270e19
victor marius <taiwosunday99@gmail.com>
2020-04-13 00:36:14 +0100

    build3

--- src/estimator.js
+++ src/estimator.js
@@ -30,8 +30,8 @@ const covid19ImpactEstimator = (data) => {
   impact.casesForVentilatorsByRequestedTime = Math.trunc(0.02 * impact.infectionsByRequestedTime);
   severeImpact.casesForVentilatorsByRequestedTime = Math.trunc(0.02 * impact.infectionsByRequestedTime);
   const severeInfectionsByRequestedTime = severeImpact.infectionsByRequestedTime;
-  impact.dollarsInFlight = Math.trunc(impact.infectionsByRequestedTime * population * 1.5) / days;
-  severeImpact.dollarsInFlight = Math.trunc(severeInfectionsByRequestedTime * population * 1.5) / days;
+  impact.dollarsInFlight = Math.trunc((impact.infectionsByRequestedTime * population * 1.5) / days);
+  severeImpact.dollarsInFlight = Math.trunc((severeInfectionsByRequestedTime * population * 1.5) / days);
 
   return {
     data,
